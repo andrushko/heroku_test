b314492ad5f0d772b06e2f3dbbf555e7cf8f16ad
Oleh Andrushko <andrushko.oleh94@gmail.com>
2018-10-04 16:32:07 +0300

    fix index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var wsServer = require('websocket').server;
 var http = require('http');
 var fs   = require('fs');
 
-var webSocketsServerPort = 8123;
+var webSocketsServerPort = 44300;
 
 var server = http.createServer(function(request, response) {
 
