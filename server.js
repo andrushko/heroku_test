50cb8c3431b80d8ef37dce6e8520305994304022
Oleh Andrushko <andrushko.oleh94@gmail.com>
2018-10-04 23:35:55 +0300

    test1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var wsServer = require('websocket').server;
 var http = require('http');
 var fs   = require('fs');
 
-var webSocketsServerPort = 8080;
+var webSocketsServerPort = 8123;
 
 var server = http.createServer(function(request, response) {
 
@@ -21,7 +21,7 @@ var server = http.createServer(function(request, response) {
 });
 
 server.listen(webSocketsServerPort, function() {
-    console.log((new Date()).toTimeString() + "Http Server is listening on port " +
+    console.log((new Date()).toTimeString() + " Http Server is listening on port " +
         webSocketsServerPort);
 });
 
@@ -32,9 +32,11 @@ var wsServer = new wsServer({
 var connections = [];
 wsServer.on('request', function(request) {
 	var connection = request.accept(null, request.origin);
-
+        console.log('res1');
+    connections.push(connection);
 	connection.on('message', function(message) {
 
+        console.log('res2');
 		var clientObj = message.utf8Data;
 
 		for (var i = 0; i < connections.length; i++) {
@@ -44,5 +46,5 @@ wsServer.on('request', function(request) {
 		
 	});
 });
-//s
+
 var alphabet = 'abcdefghijklmnopqrstuvwxyz';
