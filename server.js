ebba97824a09323aa0c2a81da96dc43cdb717d61
Oleh Andrushko <andrushko.oleh94@gmail.com>
2018-10-08 00:47:25 +0300

    editserver1

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 var wsServer = require('websocket').server;
 var http = require('http');
-var fs   = require('fs');
+var fs = require('fs');
+var ConnectedUser = require('./models/ConnectedUser');
 
 var webSocketsServerPort = process.env.PORT || 8080;;
 
 var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {
-        'Content-Type': 'text/json',
-  		'Access-Control-Allow-Origin': '*',
-  		'X-Powered-By':'nodejs'
-    });
-
-
-    fs.readFile('data.json', function(err, content){
-        response.write(content);
-        response.end();
-    });
-
+    if(request.url === '/employees'){
+        response.writeHead(200, {
+            'Content-Type': 'text/json',
+            'Access-Control-Allow-Origin': '*',
+            'X-Powered-By':'nodejs'
+        });
+        fs.readFile('data.json', function(err, content){
+            response.write(content);
+            response.end();
+        });
+    }
 });
 
 server.listen(webSocketsServerPort, function() {
@@ -31,20 +30,40 @@ var wsServer = new wsServer({
 
 var connections = [];
 wsServer.on('request', function(request) {
-    console.log('res1');
-    // Add direct messages capability
-
-	var connection = request.accept(null, request.origin);
+    var connection = request.accept(null, request.origin);
+    
     connections.push(connection);
-	connection.on('message', function(message) {
+    connection.on('message', function(message) {
 
-        console.log('res2');
-		var clientObj = message.utf8Data;
+        var clientObj = message.utf8Data;
 
-		for (var i = 0; i < connections.length; i++) {
-			connections[i].sendUTF(clientObj);
-		}
-	});
+        for (var i = 0; i < connections.length; i++) {
+            connections[i].sendUTF(clientObj);
+        }
+
+        
+    });
+// connection.on('message', function(message) {
+// 	var clientMessage = JSON.parse(message.utf8Data);
+ //        switch(clientMessage.action){
+ //            case 'ujoined': {
+
+ //                break;
+ //            };
+ //            case 'sendmes': {
+
+ //                break
+ //            };
+ //        }
+	// });
+ //    function addConnection(userId, connectionObj) {
+ //        connections.push(new ConnectedUser({id: userId}, connectionObj));
+ //    }
+ //    function sendMessage(sender, receivers, messageObj) {
+ //        for (var i = 0; i < receivers.length; i++) {
+ //            receivers[i].connection.sendUTF(messageObj);
+ //        }
+ //    }
 });
 
 var alphabet = 'abcdefghijklmnopqrstuvwxyz';
